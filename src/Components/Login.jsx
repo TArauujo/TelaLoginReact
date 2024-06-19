89ddcdc2e90a052e05c84dcf3f893ea9d285903a
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-06-19 19:10:48 -0300

    Checkbox de login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -13,7 +13,21 @@ const Login = () => {
                 <input type="password" placeholder="Senha" />
                 <FaLock className="icon" />
             </div>
+
+            <div className="recall-forget">
+              <label>
+                <input type="checkbox" />
+                Lembrar de mim?
+              </label>
+              <a href="#">Esqueceu a senha?</a>
+            </div>
+
             <button>Entrar</button>
+            <div className="signup-link">
+              <p>
+                Não tem uma conta? <a href="#">Registrar</a>
+              </p>
+            </div>
         </form>
     </div>
   )
