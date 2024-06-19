eb4d258f19154958f27abc93048ef2b6d18713b0
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-06-19 19:29:35 -0300

    Utilizando o useState para a reenderização do componente

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,23 @@
 import { FaUser, FaLock } from "react-icons/fa";
 
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();//Evita que a página seja recarregada toda vez que tiver alteração
+    console.log("teste", username, password);
+    console.log("Envio");
+  };
+
   return (
     <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}> 
             <h1>Acesse o sistema</h1>
             <div>
-                <input type="email" placeholder="E-mail"/>
+                <input type="email" placeholder="E-mail" onChange={(e) => setUsername(e.target.value)}/>
                 <FaUser className="icon" />
             </div>
             <div>
